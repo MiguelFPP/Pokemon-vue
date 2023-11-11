77b3b2ee1af66cdca6e38aa4313bdc371b9529f9
Miguel Perdomo <miguelferneyp@gmail.com>
2023-11-11 15:55:06 -0500

    Add PokemonPage and related components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template lang="es">
+    <h1>Quien es este pokemon?</h1>
+
+    <PokemonPicture />
+
+    <PokemonOptions />
+</template>
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+
+export default {
+    components: { PokemonOptions, PokemonPicture }
+}
+</script>
+<style lang="">
+    
+</style>
