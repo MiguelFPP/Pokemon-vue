79bfbe6c7e5ab47beed1bad2ecbe1aa9b5e61a9c
Miguel Perdomo <miguelferneyp@gmail.com>
2023-11-11 16:02:20 -0500

    Add Pokemon image and options styling

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,11 +1,56 @@
 <template lang="">
-    <h3>Pokemon Picture</h3>
+    <div class="pokemon-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" class="hidden-pokemon" alt="Pokemon">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" class="fade-in" alt="Pokemon">
+    </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
-<style lang="">
-    
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+    height: 200px;
+}
+
+img {
+    height: 200px;
+    position: absolute;
+    width: 100vw;
+    margin-left: -50vw;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+/* Pokemon Options */
+ul {
+    list-style-type: none;
+}
+
+li {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    margin-bottom: 10px;
+    width: 250px;
+}
+
+li:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+    display: flex;
+    justify-content: center;
+}
 </style>
