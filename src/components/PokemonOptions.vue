b6cdc96ca35ae7990fad5bb085c05a48472ca1ec
Miguel Perdomo <miguelferneyp@gmail.com>
2023-11-11 16:04:11 -0500

    Add Pokemon options to component and update styling

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,11 +1,39 @@
 <template lang="">
-    <h3>Pokemon Options</h3>
+    <div class="options-container">
+        <ul>
+            <li>Charmander</li>
+            <li>Bulbasaur</li>
+            <li>Squirtle</li>
+            <li>Pikachu</li>
+        </ul>
+    </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    /* Pokemon Options */
+ul {
+    list-style-type: none;
+}
+
+li {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    margin-bottom: 10px;
+    width: 250px;
+}
+
+li:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+    display: flex;
+    justify-content: center;
+}
 </style>
